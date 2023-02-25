21004061790f1018b1a9a1651a21073864862d92
Dauqu <sujeetsingh734321@gmail.com>
2023-02-25 20:08:14 +0530

    fdh

--- index.js
+++ index.js
@@ -49,5 +49,5 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/logout", require("./routes/logout"));
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT} !!`);
 }); //Start the server
